a220eca586f87da0d1183e975c2ca7e7f6e13db5
RYQ <1048040579@qq.com>
2022-12-10 19:48:43 +0800

    feature: delete test code

---
+++ APMr/APP.swift
@@ -0,0 +1,17 @@
+//
+//  APP.swift
+//  APMr
+//
+//  Created by 任玉乾 on 2022/11/30.
+//
+
+import SwiftUI
+
+@main
+struct APP: App {
+    var body: some Scene {
+        WindowGroup {
+            HomepageContentView()
+        }
+    }
+}
