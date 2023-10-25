b466db6aa2b3dd013ef73a8c63df4ab3cba0c80a
RYQ <1048040579@qq.com>
2023-10-25 16:25:27 +0800

    test: 添加NSTableView 示例

---
+++ APMr/Activities/Performance/Details/NSITable.swift
@@ -0,0 +1,56 @@
+//
+//  NSITable.swift
+//  APMr
+//
+//  Created by 任玉乾 on 2023/10/25.
+//
+
+import AppKit
+
+class NSITable: NSTableView {
+    public var view: NSScrollView {
+        return scrollView
+    }
+    
+    fileprivate lazy var scrollView: NSScrollView = {
+        let scrollView = NSScrollView()
+        scrollView.drawsBackground = false
+        scrollView.hasHorizontalScroller = true
+        scrollView.hasVerticalScroller = true
+        scrollView.autohidesScrollers = true
+        return scrollView
+    }()
+    
+    override init(frame frameRect: NSRect) {
+        super.init(frame: frameRect)
+        scrollView.documentView = self
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension NSTableView {
+    func reuse<T : NSTableCellView>(_ owner: Any?) -> T {
+        let id = NSUserInterfaceItemIdentifier(NSStringFromClass(T.self))
+        var cell = makeView(withIdentifier: id, owner: owner) as? T
+        
+        if cell == nil {
+            cell = T()
+            cell?.identifier = id
+        }
+        
+        return cell!
+    }
+}
+
+extension NSITable {
+    @discardableResult
+    public func add(column: String, title: String) -> NSTableColumn {
+        let column = NSTableColumn(identifier: NSUserInterfaceItemIdentifier(column))
+        column.title = title
+        addTableColumn(column)
+        return column
+    }
+}
--- APMr/Activities/Performance/Details/NSIides.swift
+++ APMr/Activities/Performance/Details/NSIides.swift
@@ -8,21 +8,145 @@
 import AppKit
 
 extension IPerformanceView.ICharts {
-    class NSISides: NSView {
+    class NSISides: NSView  {
         public var target: ISides? = nil
         
-        override func layout() {
+        private var sections = 10
+        private var currentSelection = -1
+        
+        fileprivate lazy var table: NSITable = {
+            let table = NSITable()
+            table.delegate = self
+            table.dataSource = self
+            table.selectionHighlightStyle = .none
+            table.action = #selector(onItemClicked)
 
+            return table
+        }()
+        
+        override init(frame frameRect: NSRect) {
+            super.init(frame: frameRect)
+            self.wantsLayer = true
+            addSubview(table.view)
+            
+            table.add(column: "one", title: "One")
         }
         
+        required init?(coder: NSCoder) {
+            fatalError("init(coder:) has not been implemented")
+        }
+        
+        override func layout() {
+            table.view.frame = bounds
+        }
+            
         public func refresh() {
             
         }
     }
 }
 
-extension IPerformanceView.ICharts.ISides {
-    class List: NSView {
-        lazy var tableView = NSTableView()
+extension IPerformanceView.ICharts.NSISides: NSTableViewDelegate {
+    func numberOfRows(in tableView: NSTableView) -> Int {
+        if currentSelection > -1 {
+            return sections + 10
+        }
+        return sections
+    }
+    
+    func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
+        let cell: Cell = tableView.reuse(self)
+        
+        cell.label.textColor = .blue
+        if row == currentSelection {
+            cell.label.textColor = .orange
+        }
+        
+        if currentSelection > -1, row > currentSelection, row <= currentSelection + 10 {
+            cell.label.textColor = .random
+            cell.label.stringValue = "  SUB-\(row - currentSelection - 1)"
+        } else {
+            var padding = 0
+            if currentSelection > -1, row > currentSelection + 10 {
+                padding = 10
+            }
+            cell.label.stringValue = "Section - \(row - padding)"
+        }
+        
+        return cell
+    }
+    
+    @objc private func onItemClicked() {
+        var row = table.selectedRow
+        if currentSelection > -1 {
+            if row > currentSelection, row <= currentSelection + 10 {
+                return
+            }
+            if row > currentSelection { row = row - 10 }
+        }
+        
+        var current = currentSelection
+        if current == row {
+            current = -1
+        } else {
+            current = row
+        }
+        
+        var inserts = IndexSet()
+        var removes = IndexSet()
+        
+        func indexs(_ start: Int) -> IndexSet {
+            var i = IndexSet()
+            (0 ..< 10).forEach { s in
+                i.insert(s + start + 1)
+            }
+            return i
+        }
+        
+        if current > -1 {
+            inserts = indexs(current)
+        }
+        
+        if self.currentSelection > -1 {
+            removes = indexs(self.currentSelection)
+        }
+        
+        self.currentSelection = current
+        
+        if inserts.count > 0 {
+            table.insertRows(at: inserts, withAnimation: .slideDown)
+        }
+        
+        if removes.count > 0 {
+            table.removeRows(at: removes, withAnimation: .slideUp)
+        }
+    }
+    
+    func tableViewSelectionDidChange(_ notification: Notification) {
+
+    }
+    
+}
+
+extension IPerformanceView.ICharts.NSISides: NSTableViewDataSource {
+    
+}
+
+extension IPerformanceView.ICharts.NSISides {
+    class Cell: NSTableCellView {
+        fileprivate lazy var label: NSTextField = {
+            let textField = NSTextField()
+            textField.isBordered = false
+            textField.isEditable = false
+            textField.textColor = .random
+            textField.alignment = .center
+            addSubview(textField)
+            return textField
+        }()
+        
+        override func layout() {
+            super.layout()
+            label.frame = bounds
+        }
     }
 }
